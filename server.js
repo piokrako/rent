1b2762328f95f5df1b0cd128c5ddd53b9feca877
Piotr Krakowski <piokrako@gmail.com>
2021-10-27 14:34:05 +0200

    Update Server Config

--- server.js
+++ server.js
@@ -1,30 +1,24 @@
 
-const app = require("./backend/app");
-const path = require("path");
 const express = require("express");
+const path = require("path");
 
 const port = process.env.PORT || 3000;
-// app.set("port", port);
+
 const server = express();
 server.use(express.json());
 server.use(express.urlencoded({extended: false}));
 server.use('/api', app);
-// const server = http.createServer(app);
-
-console.log('__dirname: ' + __dirname);
-console.log('static content 1: ' + path.join(__dirname, '/dist'));
-console.log('static content 2: ' + path.join(__dirname, '/frontend/dist'));
 
 if (process.env.NODE_ENV === "production") {
   console.log(
     "PROD -> server static content under: " +
       path.join(__dirname, "/frontend/dist")
   );
-  server.use(express.static(path.join(__dirname, "/frontend/dist")));
+  server.use(express.static(path.join(__dirname, "/frontend/dist/Rent")));
 } else {
   console.log("NON PROD -> run ng serve");
   require("dotenv").config();
-  server.use(express.static("frontend/dist"));
+  server.use(express.static("frontend/dist/Rent"));
 }
 
 server.listen(port, function () {
