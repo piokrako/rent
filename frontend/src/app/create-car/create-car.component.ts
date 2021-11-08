879ec4749734893f5b2283e5ec91acdd9c012045
Piotr Krakowski <piokrako@gmail.com>
2021-11-08 09:59:24 +0100

    Fix file upload blocks creating a car

--- frontend/src/app/create-car/create-car.component.ts
+++ frontend/src/app/create-car/create-car.component.ts
@@ -2,7 +2,7 @@ import { environment } from './../../environments/environment';
 import { takeUntil } from 'rxjs/operators';
 import { AdminService } from '../services/admin.service';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Subject } from 'rxjs';
 
@@ -16,7 +16,7 @@ export class CreateCarComponent implements OnInit, OnDestroy {
   fileName = '';
   fileKey = '';
   private unsubscribe = new Subject();
-
+  isButtonDisabled: boolean = false;
   path: String = environment.baseUrl;
 
   constructor(private http: HttpClient, private adminService: AdminService) { }
@@ -32,12 +32,13 @@ export class CreateCarComponent implements OnInit, OnDestroy {
   }
 
   onFileSelected(event: any) {
+    this.isButtonDisabled = true;
     const file: File = event.target.files[0];
     if (file) {
       this.fileName = file.name;
       const formData = new FormData();
       formData.append("file", file);
-      this.adminService.uploadCarImage(formData).pipe(takeUntil(this.unsubscribe)).subscribe((res: any) => { this.fileKey = res.imageKey; console.log(res) });
+      this.adminService.uploadCarImage(formData).pipe(takeUntil(this.unsubscribe)).subscribe((res: any) => { this.fileKey = res.imageKey; console.log(res); this.isButtonDisabled = false });
     }
   }
 
